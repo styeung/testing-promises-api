31423f3495b7c7b7c6a55252d2a738e9e075b9ee
Sai To Yeung <saitoyeung@gmail.com>
2017-03-01 16:08:28 -0500

    Initial commit

---
+++ app.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require 'json'
+
+items = [{id: '1', content: 'first item'}, {id: '2', content: 'second item'}]
+
+before do
+  headers 'Access-Control-Allow-Origin' => '*'
+  headers 'Access-Control-Allow-Methods' => 'POST, GET, DELETE, OPTIONS'
+  headers 'Access-Control-Allow-Headers' => 'Authorization,Accepts,Content-Type,X-CSRF-Token,X-Requested-With'
+end
+
+get '/id' do
+  JSON.generate({id: '1'})
+end
+
+get '/items/:id' do
+  item = items.select {|item| item[:id] == params['id']}.first
+  JSON.generate(item)
+end
